be8afd82518ea76c5456989b7ec19c4a74945356
Max Gfeller <max.gfeller@gmail.com>
2018-03-19 22:03:28 +0100

    Add example for styles

---
+++ examples/simple-with-style/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <span class="title">Hello there - i'm styled</span>
+  </div>
+</template>
+
+<script>
+  module.exports = {
+  }
+</script>
+
+<style lang="css">
+.title {
+  color: red;
+}
+</style>
