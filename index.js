a9a03ade11ca0054e6a04c48f733fe759c40d132
Max Gfeller <max.gfeller@gmail.com>
2018-05-21 14:49:14 +0200

    Add __TEMPLATE__ replacement method

--- index.js
+++ index.js
@@ -25,26 +25,40 @@ module.exports = function (file) {
     }
 
     var templateString = templateResult
-      ? `template: "${parseTemplateString(templateResult[1])}",` : ''
+      ? parseTemplateString(templateResult[1]) : ''
 
-    var firstPart = scriptResult[1].substring(0, (exportResult.index + exportResult[1].length))
-    var lastPart = scriptResult[1].substring((exportResult.index + exportResult[1].length) + 1)
+    // scoped style, set scoped attribute
+    if (styleResult && styleResult[3]) {
+      var insertAtPos = templateString.indexOf('>')
+      scopedDataId = cuid.slug()
+      templateString = templateString.substring(0, insertAtPos) + ` data-scoped-css=\\"${scopedDataId}\\"` + templateString.substring(insertAtPos)
+    }
 
-    if (styleResult) {
+    // Check if there is a __TEMPLATE__ placeholder, if so just put the
+    // compiled template there - otherwise insert a "template" property into
+    // the js component
+    var placeHolderIndex = scriptResult[1].indexOf('__TEMPLATE__')
+    var scriptContents = ''
+    var scopedDataId = null
+
+    if (placeHolderIndex > -1) {
+      scriptContents = scriptResult[1].replace('__TEMPLATE__', templateString)
+    } else {
+      var firstPart = scriptResult[1].substring(0, (exportResult.index + exportResult[1].length))
+      var lastPart = scriptResult[1].substring((exportResult.index + exportResult[1].length) + 1)
+      scriptContents = firstPart + `template: "${templateString}",` + lastPart
+    }
+
+    if (styleResult && (!styleResult[2] || styleResult[2] === 'css')) {
       // at the moment only proper css is supported
-      if (!styleResult[2] || styleResult[2] === 'css') {
-        if (!styleResult[3]) {
-          lastPart += `\n\nrequire('insert-css')('${styleResult[4].split('\n').join('\\n')}')`
-        } else {
-          var insertAtPos = templateString.indexOf('>')
-          var id = cuid.slug()
-          templateString = templateString.substring(0, insertAtPos) + ` data-scoped-css=\\"${id}\\"` + templateString.substring(insertAtPos)
-          lastPart += `\n\nrequire('insert-css')('${parseCSS(id, styleResult[4]).split('\n').join('\\n')}')`
-        }
+      if (!styleResult[3]) {
+        scriptContents += `\n\nrequire('insert-css')('${styleResult[4].split('\n').join('\\n')}')`
+      } else {
+        scriptContents += `\n\nrequire('insert-css')('${parseCSS(scopedDataId, styleResult[4]).split('\n').join('\\n')}')`
       }
     }
 
-    this.push(firstPart + templateString + lastPart)
+    this.push(scriptContents)
 
     next()
   })
@@ -55,6 +69,7 @@ function parseTemplateString (template) {
   str = replaceAll(str, '\n', '')
   str = replaceAll(str, '\r\n', '')
   str = replaceAll(str, '"', '\\"')
+  str = replaceAll(str, '\'', '\\\'')
 
   return str
 }
